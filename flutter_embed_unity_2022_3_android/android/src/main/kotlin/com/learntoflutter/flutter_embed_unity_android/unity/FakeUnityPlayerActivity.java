f20ccc9d2b450baa66d2dbfd227d071acf90b7f4
james <james@jamess-Mac-mini.local>
2024-03-03 16:26:17 +0000

    Add keep annotations to FakeUnityPlayerActivity in case they are needed to prevent breaking on release mode

--- flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/learntoflutter/flutter_embed_unity_android/unity/FakeUnityPlayerActivity.java
+++ flutter_embed_unity_2022_3_android/android/src/main/kotlin/com/learntoflutter/flutter_embed_unity_android/unity/FakeUnityPlayerActivity.java
@@ -1,5 +1,7 @@
 package com.learntoflutter.flutter_embed_unity_android.unity;
 
+import androidx.annotation.Keep;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 /**
@@ -14,11 +16,15 @@ public class FakeUnityPlayerActivity extends FlutterActivity implements IFakeUni
 
    // don't change the name of this variable; referenced from native code
    // See com.unity3d.player.UnityPlayerActivity.java
+   // @Keep annotation tells the minifier not to remove or rename this element during compilation
+   @Keep
    public Object mUnityPlayer;
 
    // This is called at Runtime by the flutter_embed_unity plugin.
    // It will pass an instance of com.unity3d.player.UnityPlayer
    // as the object.
+   // @Keep annotation tells the minifier not to remove or rename this element during compilation
+   @Keep
    @Override
    public void setmUnityPlayer(Object mUnityPlayer) {
       this.mUnityPlayer = mUnityPlayer;
